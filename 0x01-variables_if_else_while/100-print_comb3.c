d37a29d43f56e861239d7715a8d98ebc31bf70d2
e-one93 <kazeemismail093@gmail.com>
2022-03-12 05:14:59 -0800

    if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,13 +14,14 @@ int main(void)
 			{
 				if (m > n)
 				{
-					putchar(n);	
+					putchar(n);
 					putchar(m);
 					if (n != 56 || m != 57)
-					{			
-						putchar(',');		
+					{
+						putchar(',');
 						putchar(' ');
-					}									}
+					}
+				}
 			}
 		}
 		putchar('\n');
