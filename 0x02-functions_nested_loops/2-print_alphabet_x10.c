ff42f219fb2ba96cecf336576d84d8b25867c890
e-one93 <kazeemismail093@gmail.com>
2022-03-16 11:00:47 +0800

    nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,7 @@
 
 /**
  * print_alphabet_x10 - print alphabet 10 times
+ *
  * Return: void
  */
 
@@ -19,7 +20,7 @@ void print_alphabet_x10(void)
 			letter++;
 		}
 		_putchar('\n');
-		round++
+
+		round++;
 	}
 }
-
